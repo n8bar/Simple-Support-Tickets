3a18fcd4a2a52e19e72e0848dca29c6e2ec18eba
Nate Barlow <n8barlow@gmail.com>
2021-05-25 14:02:38 -0600

    Update TicketTest.php

--- tests/Unit/TicketTest.php
+++ tests/Unit/TicketTest.php
@@ -25,87 +25,83 @@ class TicketTest extends TestCase
 
     public function setup(): void {
         parent::setUp();
-
         $this->ticket1=Ticket::find(7);
         $this->ticket2=Ticket::find(8);
-
         $this->user=User::find(1);
+    }
+
+    public function test_user_is_Technician() {
+        echo "Test User: isTechnician\n";
         $this->assertTrue($this->user->isTechnician==1);
     }
 
     public function test_categories_setup_good() {
+        echo "\nTest Category: Login Issues";
         $this->assertEquals('Login Issues',Category::find(1)->categoryName);
+        echo "\nTest Category: Quick Question";
         $this->assertEquals('Quick Question',Category::find(2)->categoryName);
+        echo "\nTest Category: Bug Report ";
         $this->assertEquals('Bug Report',Category::find(3)->categoryName);
+        echo "\nTest Category: Feature Request ";
         $this->assertEquals('Feature Request',Category::find(4)->categoryName);
+        echo "\nTest Category: Other ";
         $this->assertEquals('Other',Category::find(5)->categoryName);
     }
 
-    public function testing_user_is_good() {
-        $this->assertTrue($this->user->isTechnician==1);
-    }
-
     public function testing_tickets_are_good() {
+        echo "\nTest Ticket1: exists and is working ";
         $this->assertTrue($this->ticket1->test());
+        echo "\nTest Ticket2: exists and is working ";
         $this->assertTrue($this->ticket2->test());
     }
 
     public function test_ticket_assigned_tech_for_unassigned_tickets() {
+        echo "\nTest Ticket1: has not been assigned ";
         $this->assertNull($this->ticket1->assignedTech());
     }
 
     public function test_ticket_assigned_tech_for_assigned() {
+        echo "\nTest Ticket2: has been assigned to user id 2 ";
         $this->assertEquals(2, $this->ticket2->assignedTech()->id);
     }
 
-/*
     public function test_ticket_status_new() {
-        //This assumes that ticket#1 has had no status changes nor has been set to new (status_id 1)
-        $ticket=Ticket::find(1);
-
-        $this->assertEquals(1, $ticket->status()->id);
+        //This assumes that ticket1 should have had no status changes nor has been set to new (status_id 1)
+        echo "\nTest Ticket Status: new ";
+        $this->assertEquals(1, $this->ticket1->status()->id);
     }
 
     public function test_ticket_status_not_new() {
-        //This assumes that ticket#14 has had status changes and has not been set to new (status_id 1)
-        $ticket=Ticket::find(14);
-
-        $this->assertTrue($ticket->status()->id > 1);
+        //This assumes that ticket2 should have had status changes and has not been set to new (status_id 1)
+        echo "\nTest Ticket Status: not new ";
+        $this->assertTrue($this->ticket2->status()->id > 1);
     }
 
-
     public function test_ticket_category_other(){
-        //This assumes that ticket#1 has category_id 5 in the database.
-        $ticket=Ticket::find(1);
-
-        $this->assertEquals(5, $ticket->category()->id);
+        //This assumes that ticket1 should have category_id 5 in the database.
+        echo "\nTest Ticket Category: Other ";
+        $this->assertEquals(5, $this->ticket1->category()->id);
     }
 
     public function test_ticket_category_bug_reports(){
-        //This assumes that ticket#14 has category_id 3 in the database.
-        $ticket=Ticket::find(14);
-
-        $this->assertEquals(3, $ticket->category()->id);
+        //This assumes that ticket2 should have category_id 3 in the database.
+        echo "\nTest Ticket Category: Bug Reports ";
+        $this->assertEquals(3, $this->ticket2->category()->id);
     }
-
 
     public function test_ticket_user(){
-        //This assumes that ticket#1 is created by user_id 3 in the database.
-        $ticket=Ticket::find(1);
-
-        $this->assertEquals(3, $ticket->user()->id);
+        //This assumes that ticket1 should have been created by user_id 4.
+        echo "\nTest Ticket user ";
+        $this->assertEquals(4, $this->ticket1->user()->id);
     }
 
-
     public function test_ticket_duration(){
-        //This assumes that tecket#6 has been set to resolved (status_id 5)
-        $ticket=Ticket::find(6);
-
-        $sc = StatusChange::where('ticket_id', $ticket->id)->
+        //This assumes that ticket2 should have been set to resolved (status_id 5)
+        $sc = StatusChange::where('ticket_id', $this->ticket2->id)->
             where('status_id',5)->
             latest()->get()->first();
-
-        $this->assertEquals($ticket->duration(), gmdate($sc->created_at->diffInSeconds($ticket->created_at)) );
+        echo "\nTest Ticket duration calculation ";
+        $this->assertEquals($this->ticket2->duration(), gmdate('H:i:s',$sc->created_at->diffInSeconds($this->ticket2->created_at)) );
     }
 /**/
 }
